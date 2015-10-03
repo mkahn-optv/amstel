af2b60f8d2d20d316bf8818b7c081912b0fc5e04
Mitch Kahn <mitch@appdelegates.com>
2015-10-03 16:01:30 -0700

    Added startup and shutdown scripts for ubuntu hosting.

---
+++ init-ubuntu.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+### BEGIN INIT INFO
+# Provides: Amstel
+# Required-Start: $remote_fs $syslog
+# Required-Stop: $remote_fs $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Amstel
+# Description: This file starts and stops Amstel server
+#
+### END INIT INFO
+
+AMSTEL_DIR=/opt/amstel/
+
+case "$1" in
+ start)
+   su mkahn -c AMSTEL_DIR/startup.sh
+   ;;
+ stop)
+   su joe -c AMSTEL_DIR/startup.sh
+   sleep 10
+   ;;
+ restart)
+   su joe -c AMSTEL_DIR/shutdown.sh
+   sleep 20
+   su joe -c AMSTEL_DIR/startup.sh
+   ;;
+ *)
+   echo "Usage: amstel {start|stop|restart}" >&2
+   exit 3
+   ;;
+esac
