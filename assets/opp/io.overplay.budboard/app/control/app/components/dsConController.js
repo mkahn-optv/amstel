86574a7d7e34b90124ae82dfcc61110a9b9324c8
Mitch Kahn <mitch@appdelegates.com>
2015-12-08 11:13:27 -0800

    Bud Board App

---
+++ assets/opp/io.overplay.budboard/app/control/app/components/dsConController.js
@@ -0,0 +1,49 @@
+/**
+ * Created by mkahn on 4/28/15.
+ */
+
+app.controller( "dsConController",
+    function ( $scope, $timeout, $http, $log, optvModel ) {
+
+        $log.info( "Loading dsConController" );
+
+        var logLead = "DS Control App: ";
+        $scope.inboundMessageArray = [];
+        $scope.messageArray = [];
+
+        function modelUpdate( data ) {
+
+            $log.info( logLead + " got a model update: " + angular.toJson( data ) );
+            $scope.messageArray = data.messages;
+
+
+        }
+
+        function inboundMessage( msg ) {
+            $log.debug( logLead + "Inbound message..." );
+        }
+
+        function initialize() {
+
+            optvModel.init( {
+                appName:         "io.overplay.budboard",
+                dataCallback:    modelUpdate,
+                messageCallback: inboundMessage
+            } );
+
+        }
+
+        $scope.add = function () {
+            $scope.messageArray.push( $scope.input.newMsg );
+            $scope.input.newMsg = '';
+        }
+
+        $scope.done = function () {
+            optvModel.messages = $scope.messageArray;
+            optvModel.save();
+        }
+
+
+        initialize();
+
+    } );
